6f0c56d9f5742416f56d23214bda16718955daa7
devdewa <uchiasasuke123.sp@gmail.com>
2023-12-27 00:37:33 +0700

    feat: nuxtui config

---
+++ app.config.ts
@@ -0,0 +1,18 @@
+export default defineAppConfig({
+  ui: {
+    primary: 'slate',
+    container: {
+      constrained: 'max-w-screen-2xl',
+    },
+    card: {
+      background: '',
+      rounded: '',
+      divide: '',
+      shadow: '',
+      ring: '',
+      body: {
+        padding: '',
+      },
+    },
+  },
+});
