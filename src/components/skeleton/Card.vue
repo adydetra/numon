5a3bb13393f36e030bc9594648a8b005fcb32344
devdewa <uchiasasuke123.sp@gmail.com>
2024-02-16 19:19:21 +0700

    chore: change utility name to size

--- src/components/skeleton/Card.vue
+++ src/components/skeleton/Card.vue
@@ -1,7 +1,7 @@
 <template>
   <div mx-auto h-20 max-w-sm w-full md:h-51>
     <div flexcenter flex-col animate-pulse space-y-4 md:space-y-12>
-      <div h-10 w-10 rounded-full bg-slate-400 md:h-20 md:w-20 />
+      <div size-10 rounded-full bg-slate-400 md:size-20 />
       <div h-2 w-20 rounded bg-slate-400 md:w-28 />
     </div>
   </div>
