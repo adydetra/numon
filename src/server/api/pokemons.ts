960c11a0881bb19919da3272bcb1954443bf4694
devdewa <uchiasasuke123.sp@gmail.com>
2024-02-16 19:17:34 +0700

    chore: lint

--- src/server/api/pokemons.ts
+++ src/server/api/pokemons.ts
@@ -1,12 +1,14 @@
-export default defineEventHandler(async () => {
-  const { results } = await $fetch('https://pokeapi.co/api/v2/pokemon/?limit=9');
+export default defineEventHandler(
+  async () => {
+    const { results } = await $fetch('https://pokeapi.co/api/v2/pokemon/?limit=9');
 
-  return results.map((el) => {
-    const id = el?.url?.split('/')[6];
-    return {
-      id,
-      name: el.name,
-      image: `https://raw.githubusercontent.com/PokeAPI/sprites/master/sprites/pokemon/versions/generation-v/black-white/animated/${id}.gif`,
-    };
-  });
-});
+    return results.map((el) => {
+      const id = el?.url?.split('/')[6];
+      return {
+        id,
+        name: el.name,
+        image: `https://raw.githubusercontent.com/PokeAPI/sprites/master/sprites/pokemon/versions/generation-v/black-white/animated/${id}.gif`,
+      };
+    });
+  },
+);
